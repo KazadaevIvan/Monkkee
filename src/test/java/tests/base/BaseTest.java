37aedcf4ed35a2d4347aeb9ca878bca8fbb9f836
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2022-08-19 12:54:56 +0300

    Add chromedriver.exe

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,15 +1,13 @@
 package tests.base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import steps.LoginPageSteps;
+import utils.CapabilitiesGenerator;
 import utils.PropertyReader;
 
 import java.util.concurrent.TimeUnit;
@@ -21,19 +19,10 @@ public class BaseTest {
     private WebDriver driver;
     protected LoginPageSteps loginPageSteps;
 
-    @BeforeClass
-    public void before() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @Step("Open browser")
     @BeforeMethod
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary("/usr/bin/google-chrome");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         loginPageSteps = new LoginPageSteps(driver);
