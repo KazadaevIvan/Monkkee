fef1d7154ae53c3d3641da8fc08bcb4a6c15ca27
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2022-08-23 14:08:36 +0300

    Add EntryPageSteps

---
+++ src/test/java/steps/EntryPageSteps.java
@@ -0,0 +1,38 @@
+package steps;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import pages.EntryPage;
+
+import static utils.AllureUtils.takeScreenshot;
+
+public class EntryPageSteps {
+    private EntryPage entryPage;
+
+    public EntryPageSteps(WebDriver driver) {
+        this.entryPage = new EntryPage(driver);
+    }
+
+    @Step("Input Entry text {text}")
+    public EntryPageSteps inputEntryText(String text) {
+        entryPage
+                .isPageOpened()
+                .inputText(text)
+                .waitForEntryToBeSaved();
+        takeScreenshot(entryPage.driver);
+        return this;
+    }
+
+    @Step("Get entry id")
+    public String getEntryId() {
+        return entryPage.getEntryId();
+    }
+
+    @Step("Open Home page")
+    public EntryPageSteps openHomePage() {
+        entryPage
+                .clickBackToOverViewButton();
+        takeScreenshot(entryPage.driver);
+        return this;
+    }
+}
