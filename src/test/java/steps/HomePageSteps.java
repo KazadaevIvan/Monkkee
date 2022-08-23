40882bd8e683f275bdf01ca6bc5764ad5b9c9b87
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2022-08-23 14:09:29 +0300

    Add HomePageSteps

---
+++ src/test/java/steps/HomePageSteps.java
@@ -0,0 +1,31 @@
+package steps;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import pages.HomePage;
+
+import static utils.AllureUtils.takeScreenshot;
+import static org.testng.Assert.assertEquals;
+
+public class HomePageSteps {
+    private HomePage homePage;
+
+    public HomePageSteps(WebDriver driver) {
+        this.homePage = new HomePage(driver);
+    }
+
+    @Step("Start to create new entry")
+    public HomePageSteps startToCreateNewEntry() {
+        homePage.clickCreateAnEntryButton();
+        takeScreenshot(homePage.driver);
+        return this;
+    }
+
+    @Step("Verify entry with id {id} has text {text}")
+    public HomePageSteps verifyEntryWithIdHasText(String id, String text) {
+        homePage.isPageOpened();
+        assertEquals(homePage.getEntryWithIdText(id), text);
+        takeScreenshot(homePage.driver);
+        return this;
+    }
+}
