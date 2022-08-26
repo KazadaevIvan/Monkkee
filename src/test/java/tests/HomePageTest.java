760894555f5006b76a39bf56fc8be7edecc0129c
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2022-08-26 14:12:58 +0300

    Add BeforeMethod

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,21 +1,27 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
 
+import static utils.RandomNumberGenerator.getRandomNumber;
+
 public class HomePageTest extends BaseTest {
+
+    @BeforeMethod
+    public void login() {
+        loginPageSteps.login(USERNAME, PASSWORD);
+    }
 
     @Test(description = "Validation that new entry can be created")
     public void newEntryShouldBeCreated() {
-        loginPageSteps.login(USERNAME, PASSWORD);
         homePageSteps.startToCreateNewEntry();
 
-        String expectedResult = "Hey! This is the first entry";
+        String expectedResult = "Hey! This is the first entry " + getRandomNumber();
 
         entryPageSteps.inputEntryText(expectedResult);
 
         String entryId = entryPageSteps.getEntryId();
-
         entryPageSteps.openHomePage();
         homePageSteps.verifyEntryWithIdHasText(entryId, expectedResult);
     }
