da1e4743f8f9aae2ebd69ddf75e01f988792e106
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2022-08-26 18:57:05 +0300

    Reformat output text

--- src/test/java/tests/base/TestListener.java
+++ src/test/java/tests/base/TestListener.java
@@ -15,25 +15,25 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        System.out.printf("======================================== STARTING TEST %s ========================================%n", iTestResult.getName());
+        System.out.printf("==================== STARTING TEST %s ====================%n", iTestResult.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        System.out.printf("======================================== FINISHED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
+        System.out.printf("==================== FINISHED TEST %s Duration: %ss ====================%n", iTestResult.getName(),
                 getExecutionTime(iTestResult));
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.printf("======================================== FAILED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
+        System.out.printf("==================== FAILED TEST %s Duration: %ss ====================%n", iTestResult.getName(),
                 getExecutionTime(iTestResult));
         takeScreenshot(iTestResult);
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-        System.out.printf("======================================== SKIPPING TEST %s ========================================%n", iTestResult.getName());
+        System.out.printf("==================== SKIPPING TEST %s ====================%n", iTestResult.getName());
         takeScreenshot(iTestResult);
     }
 
