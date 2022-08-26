2d55e43b43d954ebfc8171ac9d13383870cde13f
Ivan Kazadaew <ivan.kazadaew@leverx.com>
2022-08-26 16:53:13 +0300

    Delete BeforeMethod & AfterMethod

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,7 +1,5 @@
 package tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
 
@@ -9,13 +7,10 @@ import static utils.RandomNumberGenerator.getRandomNumber;
 
 public class HomePageTest extends BaseTest {
 
-    @BeforeMethod(description = "Login user")
-    public void login() {
-        loginPageSteps.login(USERNAME, PASSWORD);
-    }
-
     @Test(description = "Validation that new entry can be created")
     public void newEntryShouldBeCreated() {
+        loginPageSteps.login(USERNAME, PASSWORD);
+
         homePageSteps.startToCreateNewEntry();
 
         String expectedResult = "Hey! This is the first entry " + getRandomNumber();
@@ -25,10 +20,5 @@ public class HomePageTest extends BaseTest {
         String entryId = entryPageSteps.getEntryId();
         entryPageSteps.openHomePage();
         homePageSteps.verifyEntryWithIdHasText(entryId, expectedResult);
-    }
-
-    @AfterMethod(description = "Logout user")
-    public void logout() {
-        homePageSteps.logout();
     }
 }
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
 
@@ -9,10 +8,5 @@ public class LoginPageTest extends BaseTest {
     @Test(description = "Validation that user could login with valid credentials")
     public void userShouldBeLoggedIn() {
         loginPageSteps.login(USERNAME, PASSWORD);
-    }
-
-    @AfterMethod(description = "Logout user")
-    public void logout() {
-        homePageSteps.logout();
     }
 }
